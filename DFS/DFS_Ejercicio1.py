b5422e82090ec6f743e0757496421b1b3e7b8c3d
Jessie Contreras <jncontreras@espe.edu.ec>
2022-06-02 17:19:19 -0500

    Actualizaciones

--- DFS/DFS_Ejercicio1.py
+++ DFS/DFS_Ejercicio1.py
@@ -1,3 +1,4 @@
+
 #Primero en Entrar, Primero en Salir
 from queue import Queue
 
@@ -11,41 +12,44 @@ class grafo:
         self.m_dirigrafo= dirigrafo #Grafo dirigido 
        
         self.m_adj_lista = {nodo: set() for nodo in self.m_nodos} #Se crea una lista adyacente mediante un diccionario.
-        
-def add_grafo(self, nodo1, nodo2, peso=1): #Se inicializa los nodos, diriguiendolos al borde del grafo
+   
+    def add_grafo(self, nodo1, nodo2, peso=1): #Se inicializa los nodos, diriguiendolos al borde del grafo
         self.m_adj_lista[nodo1].add((nodo2, peso))#hace funcion al nodo no dirigido.
        
         if not self.m_dirigrafo:
             self.m_adj_lista[nodo2].add((nodo1, peso)) # tomamo el nodo 2 y el nodo 1 se ubica al borde
     
-def print_adj_lista(self):# Representacion grafica def 
+    def print_adj_lista(self):# Representacion grafica def 
         #Numeros de nodos claves
         for llave in self.m_adj_lista.keys(): 
             print("nodo", llave, ": ", self.m_adj_lista[llave])# Imprime los nodos con la llave
-
-
-#actua
+ 
+ 
+ #actua
+ #actua
  #Agregamos el nodo de inicio al comienzo de nuestra ruta transversal y lo marcamos como 
  # visitado al agregarlo a un conjunto de nodos visitados
-def dfs(self, inicio, objetivo, ruta = [], visita = set()): #creamos una lsita vacia de la ruta y el objetivo   
-        ruta.append(inicio) 
+    def dfs(self, inicio, objetivo, ruta = [], visita = set()):
+        ruta.append(inicio)
         #Agregamos el nodo de inicio al comienzo de nuestra ruta
         visita.add(inicio)
-        if inicio == objetivo:  #y nos preguntamos si el inicio es igual al objetivo
-            return ruta # y nos devuele la ruta
+        if inicio == objetivo: #y nos preguntamos si el inicio es igual al objetivo
+            return ruta# y nos devuele la ruta
         #llamamos a la función recursivamente para cada uno de ellos
-        for (neighbour, peso) in self.m_adj_lista[inicio]: 
+        for (neighbour, peso) in self.m_adj_lista[inicio]:
             if neighbour not in visita:
                 resultado = self.dfs(neighbour, objetivo, ruta, visita)
-                if resultado is not None: 
+                if resultado is not None:
                     #guardamos una referencia al resultado
                     return resultado
-                #hemos encontrado nuestro nodo de destino y 
+ #hemos encontrado nuestro nodo de destino y 
                 # devolvemos la ruta transversal como resultado.
         ruta.pop()
         return None
 
-if __name__ == "__main__": # 
+
+if __name__ == "__main__":
+    
     #Esto creará la instancia del Grafo de gráfico no dirigido que representa con un N nodos
     grafo = grafo(9, dirigrafo = False) #Ingresamos el numero de datos a imprimir
     print("Busqueda por Profundidad")
@@ -78,4 +82,3 @@ if __name__ == "__main__": #
     traversal_path = []
     traversal_path = grafo.dfs(0, 8)
     print(f" El camino transversal del nodo 1 al 8 nodo es {dato1, dato2, dato6, dato4, dato6, dato5, dato7, dato8}")
-
