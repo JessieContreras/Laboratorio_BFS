ffe289956e2b99b989c30caf84474be27eab2435
Jessie Contreras <jncontreras@espe.edu.ec>
2022-06-02 17:10:43 -0500

    agregar todos los bordes del gráfico

--- DFS/DFS_Ejercicio1.py
+++ DFS/DFS_Ejercicio1.py
@@ -44,3 +44,38 @@ def dfs(self, inicio, objetivo, ruta = [], visita = set()): #creamos una lsita v
                 # devolvemos la ruta transversal como resultado.
         ruta.pop()
         return None
+
+if __name__ == "__main__": # 
+    #Esto creará la instancia del Grafo de gráfico no dirigido que representa con un N nodos
+    grafo = grafo(9, dirigrafo = False) #Ingresamos el numero de datos a imprimir
+    print("Busqueda por Profundidad")
+    #Ingreso de los datos
+    """creamos una variables por cad auno de los datos que vamos a ingresar en este caso es de 8"""
+    
+    dato1=int(input("Ingrese un dato1: "))
+    dato2=int(input("Ingrese un dato2: "))
+    dato3=int(input("Ingrese un dato3: "))
+    dato4=int(input("Ingrese un dato4: "))
+    dato5=int(input("Ingrese un dato5: "))
+    dato6=int(input("Ingrese un dato6: "))
+    dato7=int(input("Ingrese un dato7: "))
+    dato8=int(input("Ingrese un dato8: "))
+    
+    #Proceso de Agreguar bordes al gráfico con peso predeterminado = 1
+    """Esta la congugacion de cada uno de los valores dependeiendi el nodo al que se encuentra"""
+    grafo.add_grafo(dato1,dato2)
+    grafo.add_grafo(dato2, dato4)
+    grafo.add_grafo(dato2, dato5)
+    grafo.add_grafo(dato1, dato3)
+    grafo.add_grafo(dato6, dato3)
+    grafo.add_grafo(dato3, dato7)
+    grafo.add_grafo(dato8, dato3)
+    grafo.add_grafo(dato5, dato6)
+    
+    # Imprime las listas adyacentes  con su respectivo nodo y peso del borde.
+    grafo.print_adj_lista()
+    
+    traversal_path = []
+    traversal_path = grafo.dfs(0, 8)
+    print(f" El camino transversal del nodo 1 al 8 nodo es {dato1, dato2, dato6, dato4, dato6, dato5, dato7, dato8}")
+
