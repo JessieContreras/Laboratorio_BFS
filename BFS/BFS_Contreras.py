05df16bf6a66dfe87a4ff646d933a32b77b25e6f
Jessie Contreras <jncontreras@espe.edu.ec>
2022-06-02 12:33:54 -0500

    Ininializando los metodos

---
+++ BFS/BFS_Contreras.py
@@ -0,0 +1,10 @@
+#Primero en Entrar, Primero en Salir
+from queue import Queue
+
+#Crea una función llamada Grafo.
+class grafo:
+    
+    def __init__(self, numerosNodos, dirigrafo=True): #inicializamos los metodo.
+        self.m_numero_de_nodos = numerosNodos #ingreso de los atributos del nodo.
+        self.m_nodos = range(self.m_numero_de_nodos) #ingreso del rango del nodo
+       
