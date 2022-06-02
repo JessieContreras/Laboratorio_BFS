553e01b08c9b794f527e0c03fd0e205ec74bd4c7
Jessie Contreras <jncontreras@espe.edu.ec>
2022-06-02 16:35:49 -0500

    nicializa los nodos, diriguiendolos al borde del grafo y la representación gráfica

--- DFS/DFS_Ejercicio1.py
+++ DFS/DFS_Ejercicio1.py
@@ -11,3 +11,15 @@ class grafo:
         self.m_dirigrafo= dirigrafo #Grafo dirigido 
        
         self.m_adj_lista = {nodo: set() for nodo in self.m_nodos} #Se crea una lista adyacente mediante un diccionario.
+        
+def add_grafo(self, nodo1, nodo2, peso=1): #Se inicializa los nodos, diriguiendolos al borde del grafo
+        self.m_adj_lista[nodo1].add((nodo2, peso))#hace funcion al nodo no dirigido.
+       
+        if not self.m_dirigrafo:
+            self.m_adj_lista[nodo2].add((nodo1, peso)) # tomamo el nodo 2 y el nodo 1 se ubica al borde
+    
+def print_adj_lista(self):# Representacion grafica def 
+        #Numeros de nodos claves
+        for llave in self.m_adj_lista.keys(): 
+            print("nodo", llave, ": ", self.m_adj_lista[llave])# Imprime los nodos con la llave
+ 
