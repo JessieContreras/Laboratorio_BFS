50c1230d4561de04cf75a84bbe5532d1c2193da4
Jessie Contreras <jncontreras@espe.edu.ec>
2022-06-02 12:35:02 -0500

    crea una lista adyacente

--- BFS/BFS_Contreras.py
+++ BFS/BFS_Contreras.py
@@ -7,4 +7,12 @@ class grafo:
     def __init__(self, numerosNodos, dirigrafo=True): #inicializamos los metodo.
         self.m_numero_de_nodos = numerosNodos #ingreso de los atributos del nodo.
         self.m_nodos = range(self.m_numero_de_nodos) #ingreso del rango del nodo
+        self.m_dirigrafo= dirigrafo #Grafo dirigido 
        
+        self.m_adj_lista = {nodo: set() for nodo in self.m_nodos} #Se crea una lista adyacente mediante un diccionario.
+   
+    def add_edgrafo(self, nodo1, nodo2, peso=1): #Se inicializa los nodos, diriguiendolos al borde del grafo
+        self.m_adj_lista[nodo1].add((nodo2, peso))#hace funcion al nodo no dirigido.
+        if not self.m_dirigrafo:
+            self.m_adj_lista[nodo2].add((nodo1, peso)) # tomamo el nodo 2 y el nodo 1 se ubica al borde
+    
